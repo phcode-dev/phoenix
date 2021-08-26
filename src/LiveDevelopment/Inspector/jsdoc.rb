3deda6586ec44d3b0b393e732c8ec726338b702b
Charly <charly@core.ai>
2021-08-26 16:45:26 +0530

    Updating copyright notice

--- src/LiveDevelopment/Inspector/jsdoc.rb
+++ src/LiveDevelopment/Inspector/jsdoc.rb
@@ -1,26 +1,27 @@
 #!/bin/ruby
 #
 #
-# Copyright (c) 2012 - present Adobe Systems Incorporated. All rights reserved.
-#  
+# Modified Work Copyright (c) 2021 - present core.ai . All rights reserved.
+# Original work Copyright (c) 2012 - 2021 Adobe Systems Incorporated. All rights reserved.
+#
 # Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"), 
-# to deal in the Software without restriction, including without limitation 
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-# and/or sell copies of the Software, and to permit persons to whom the 
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following conditions:
-#  
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-#  
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-# 
+#
 #
 # This script generates debugger documentation from Inspector.json
 #
@@ -82,7 +83,7 @@ class JSDoc
 	end
 
 	def run
-		open do 
+		open do
 			writeDocument do
 				writeTOC
 				@in["domains"].each { |domain| writeDomain domain }
