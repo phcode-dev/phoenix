4eae0e9a9ee54b0e4377acafe37655e0a7ba4f4e
Arun Bose <arun@core.ai>
2021-10-31 18:29:59 +0530

    Performance improvement of file tree: select file on mouse down event instead of click.

--- src/project/FileTreeView.js
+++ src/project/FileTreeView.js
@@ -379,6 +379,7 @@ define(function (require, exports, module) {
             if (this.props.entry.get("rename")) {
                 return;
             }
+            this.selectNode(e);
         }
     };
 
@@ -569,6 +570,28 @@ define(function (require, exports, module) {
             }
             e.stopPropagation();
             e.preventDefault();
+        },
+
+        /**
+         * select the current node in the file tree on mouse down event on files.
+         * This is to increase click responsiveness of file tree.
+         */
+        selectNode: function (e) {
+            if (e.button !== LEFT_MOUSE_BUTTON) {
+                return;
+            }
+
+            var language = LanguageManager.getLanguageForPath(this.myPath()),
+                doNotOpen = false;
+            if (language && language.isBinary() && "image" !== language.getId() &&
+                FileUtils.shouldOpenInExternalApplication(
+                    FileUtils.getFileExtension(this.myPath()).toLowerCase()
+                )
+            ) {
+                doNotOpen = true;
+            }
+            this.props.actions.setSelected(this.myPath(), doNotOpen);
+            render();
         },
 
         /**
@@ -835,6 +858,14 @@ define(function (require, exports, module) {
             }
             event.stopPropagation();
             event.preventDefault();
+        },
+
+        /**
+         * select the current node in the file tree
+         */
+        selectNode: function (e) {
+            // Do nothing for folders on keydown event. Only expand the file tree on click event
+            // to prevent jarring directory accordion expansion in ui.
         },
 
         /**
