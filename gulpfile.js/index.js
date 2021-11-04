d89813ee7818bd81887f701462e9c0154dd863de
Arun Bose <arun@core.ai>
2021-11-04 21:08:32 +0530

    build: npm release scripts and doc update https://github.com/aicore/phoenix/issues/223

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,15 +1,30 @@
 /* eslint-env node */
 
 const del = require('del');
-var webserver = require('gulp-webserver');
+const webserver = require('gulp-webserver');
+const uglify = require('gulp-uglify');
+const pipeline = require('readable-stream').pipeline;
 const { src, dest, series } = require('gulp');
+const concat = require('gulp-concat');
+
+function cleanDist() {
+    return del(['dist']);
+}
 
 function cleanAll() {
     return del(['node_modules', 'dist']);
 }
 
+function release() {
+    return pipeline(
+        src(['src/**/*.js', '!src/extensions/**', '!src/thirdparty/**'],  { sourcemaps: true })
+            .pipe(concat('all.js')),
+        uglify(),
+        dest('dist/',  { sourcemaps: true })
+    );
+}
+
 function build(cb) {
-    // body omitted
     cb();
 }
 
@@ -34,7 +49,9 @@ function serveExternal() {
 
 
 exports.build = build;
-exports.clean = series(cleanAll);
+exports.clean = series(cleanDist);
+exports.reset = series(cleanAll);
+exports.release = series(cleanDist, build, release);
 exports.serve = series(build, serve);
 exports.serveExternal = series(build, serveExternal);
 exports.default = series(build);
