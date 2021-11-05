0be9bd8a0861755fa243ced752039fe4ab76a9cc
Arun Bose <arun@core.ai>
2021-11-05 15:11:15 +0530

    build: npm release scripts and doc update https://github.com/aicore/phoenix/issues/223

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,10 +2,14 @@
 
 const del = require('del');
 const webserver = require('gulp-webserver');
-const uglify = require('gulp-uglify');
-const pipeline = require('readable-stream').pipeline;
 const { src, dest, series } = require('gulp');
-const concat = require('gulp-concat');
+// This plugin concatenates any number of CSS and JavaScript files into a single file
+const useref = require('gulp-useref');
+// Requires the gulp-uglify plugin for minifying js files
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+// Requires the gulp-cssnano plugin for minifying css files
+const cssnano = require('gulp-cssnano');
 
 function cleanDist() {
     return del(['dist']);
@@ -15,13 +19,25 @@ function cleanAll() {
     return del(['node_modules', 'dist']);
 }
 
+/**
+ * TODO: Release scripts to merge and min src js/css/html resources into dist.
+ * Links that might help:
+ * for less compilation:
+ * https://stackoverflow.com/questions/27627936/compiling-less-using-gulp-useref-and-gulp-less
+ * https://www.npmjs.com/package/gulp-less
+ * Minify multiple files into 1:
+ * https://stackoverflow.com/questions/26719884/gulp-minify-multiple-js-files-to-one
+ * https://stackoverflow.com/questions/53353266/minify-and-combine-all-js-files-from-an-html-file
+ * @returns {*}
+ */
 function release() {
-    return pipeline(
-        src(['src/**/*.js', '!src/extensions/**', '!src/thirdparty/**'],  { sourcemaps: true })
-            .pipe(concat('all.js')),
-        uglify(),
-        dest('dist/',  { sourcemaps: true })
-    );
+    return src('src/*.html')
+        .pipe(useref())
+        // Minifies only if it's a JavaScript file
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(dest('dist'));
 }
 
 function build(cb) {
