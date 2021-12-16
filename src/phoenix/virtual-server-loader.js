c86d784bb9502be61e38d16b90ba27fa21e00911
Arun Bose <arun@core.ai>
2021-12-16 19:06:24 +0530

    chore: remove PWA code as it will be moved to build step later https://github.com/aicore/phoenix/issues/102

--- src/phoenix/virtual-server-loader.js
+++ src/phoenix/virtual-server-loader.js
@@ -67,53 +67,5 @@ if ('serviceWorker' in navigator) {
     });
 
     wb.register();
-    navigator.serviceWorker.register("service-worker.js");
-    initPWA();
 }
 
-
-let deferredPrompt;
-function initPWA () {
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent the mini-infobar from appearing on mobile
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-        // Update UI notify the user they can install the PWA
-        showInstallPromotion();
-        // Optionally, send analytics event that PWA install promo was shown.
-        console.log(`'beforeinstallprompt' event was fired.`);
-    });
-}
-
-const btn = window.document.createElement("button");
-
-function showInstallPromotion() {
-    btn.innerHTML = "Install App";
-    btn.type = "submit";
-    btn.name = "installBtn";
-    btn.style.position = 'fixed';
-    btn.style.bottom = '20px';
-    btn.style.right = '100px';
-    btn.style.padding = '10px';
-    btn.style.background = 'whitesmoke';
-    btn.style.boxShadow = '0 0 11px rgb(33 33 33 / 20%)';
-    btn.addEventListener('click', async () => {
-        // Hide the app provided install promotion
-        hideInstallPromotion();
-        // Show the install prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        const { outcome } = await deferredPrompt.userChoice;
-        // Optionally, send analytics event with outcome of user choice
-        console.log(`User response to the install prompt: ${outcome}`);
-        // We've used the prompt, and can't use it again, throw it away
-        deferredPrompt = null;
-    });
-    setTimeout(() => {window.document.body.appendChild(btn);}, 2000);
-}
-
-function hideInstallPromotion() {
-    window.document.body.removeChild(btn);
-}
