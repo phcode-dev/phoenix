5b3d941d8f7361e66b08238c1a7f1865df039850
Arun Bose <arun@core.ai>
2022-01-02 18:39:55 +0530

    feat: changed working set split view icon

--- src/project/SidebarView.js
+++ src/project/SidebarView.js
@@ -127,7 +127,6 @@ define(function (require, exports, module) {
      */
     function _updateUIStates() {
         var spriteIndex,
-            ICON_CLASSES = ["splitview-icon-none", "splitview-icon-vertical", "splitview-icon-horizontal"],
             layoutScheme = MainViewManager.getLayoutScheme();
 
         if (layoutScheme.columns > 1) {
@@ -137,10 +136,6 @@ define(function (require, exports, module) {
         } else {
             spriteIndex = 0;
         }
-
-        // SplitView Icon
-        $splitViewMenu.removeClass(ICON_CLASSES.join(" "))
-                      .addClass(ICON_CLASSES[spriteIndex]);
 
         // SplitView Menu
         _cmdSplitNone.setChecked(spriteIndex === 0);
