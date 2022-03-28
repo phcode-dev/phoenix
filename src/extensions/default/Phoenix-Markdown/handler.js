40595d3f0b6346adee34b777db8406c2d2e31eb2
dhruvpeter <dhruveldhopeter@gmail.com>
2022-03-28 21:39:15 +0530

    Modify the copyright string

--- src/extensions/default/Phoenix-Markdown/handler.js
+++ src/extensions/default/Phoenix-Markdown/handler.js
@@ -1,7 +1,7 @@
 /*
  * GNU AGPL-3.0 License
  *
- * Modified Work Copyright (c) 2021 - present core.ai . All rights reserved.
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
  * Original work Copyright (c) 2016 Alan Hohn
  *
  * This program is free software: you can redistribute it and/or modify it
