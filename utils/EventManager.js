fe9431b268316f590525c2fc67d5e72d85065f6c
abose <abose@users.noreply.github.com>
2022-06-08 15:02:09 +0000

    deploy: 4961dc0696671d50a1e7d2a096716a6472058bca

--- utils/EventManager.js
+++ utils/EventManager.js
@@ -21,18 +21,52 @@
 /*global less */
 // jshint ignore: start
 
+// @INCLUDE_IN_API_DOCS
+
 /**
- * Exposes named events and triggers a named event manager as required.
- * See <doc link here for more details on how to use this API>
+ * The global EventManager can be used to register named EventDispatchers so that events
+ * can be triggered from anywhere without using require context.
+ *
+ * A global `window.EventManager` object is made available in phoenix that can be called anytime after AppStart.
+ *
+ * ## Usage
+ * For Eg. Let's say we have an extension `drawImage` installed that wants to expose custom functionality to phoenix.
+ * The Extension will first register named EventHandler like this:
+ * ```js
+ * // in drawImage/someExtensionModule.js module within the extension, do the following:
+ * const EventDispatcher = brackets.getModule("utils/EventDispatcher"),
+ * EventManager = brackets.getModule("utils/EventManager");
+ * EventDispatcher.makeEventDispatcher(exports);
+ *
+ * EventManager.registerEventHandler("drawImageHandler", exports);
+ * ```
+ * Once the event handler is registered, we can trigger events on the named handler anywhere in phoenix
+ * (inside or outside the extension) by using:
+ *
+ * ```js
+ * EventManager.triggerEvent("drawImageHandler", "someEventName", "param1", "param2", ...);
+ * ```
+ * @module utils/EventManager
  */
 define(function (require, exports, module) {
 
     const _eventHandlerMap = {};
     /**
-     * Returns true is an interface of the given name exists.
+     * Registers a named EventHandler. Event handlers are created using the call:
+     * `EventDispatcher.makeEventDispatcher(Command.prototype);`
+     *
+     * @example <caption>To register a close dialogue event handler in an extension:</caption>
+     * // in close-dialogue.js module winthin the extension, do the following:
+     * const EventDispatcher = brackets.getModule("utils/EventDispatcher"),
+     * EventDispatcher.makeEventDispatcher(exports);
+     *
+     * EventManager.registerEventHandler("closeDialogueHandler", exports);
+     * // Once the event handler is registered, see triggerEvent API on how to raise events
+     *
      * @param {string} handlerName a unique name of the handler.
      * @param {object} eventDispatcher An EventDispatcher that will be used to trigger events.
      * @return {boolean}
+     * @type {function}
      */
     function registerEventHandler(handlerName, eventDispatcher) {
         if(_eventHandlerMap[handlerName]){
@@ -45,6 +79,7 @@ define(function (require, exports, module) {
      * Returns true is an EventHandler of the given name exists.
      * @param {string} handlerName
      * @return {boolean}
+     * @type {function}
      */
     function isExistsEventHandler(handlerName) {
         return _eventHandlerMap[handlerName] !== undefined;
@@ -52,9 +87,16 @@ define(function (require, exports, module) {
 
     /**
      * Triggers an event on the named event handler.
-     * @param handlerName
-     * @param eventName
+     *
+     * @example <caption>To trigger an event to the `closeDialogue` event handler registered above</caption>
+     * // anywhere in code, do the following:
+     * const EventManager = brackets.getModule("utils/EventManager");
+     * EventManager.triggerEvent("closeDialogueHandler", "someEvent", "param1", "param2", ...);
+     *
+     * @param {string} handlerName
+     * @param eventName the event name as recognised by the handler. this is usually a string.
      * @param eventParams Can be a comma seperated list of args or a single argument.
+     * @type {function}
      */
     function triggerEvent(handlerName, eventName, ...eventParams) {
         let handler = _eventHandlerMap[handlerName];
