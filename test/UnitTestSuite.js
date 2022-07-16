f096383bf961623b621f5be04a6d161c53300dae
Arun Bose <arun@core.ai>
2022-07-16 17:51:21 +0530

    test: enable QuickSearchField-test

--- test/UnitTestSuite.js
+++ test/UnitTestSuite.js
@@ -61,6 +61,7 @@ define(function (require, exports, module) {
     require("spec/Pane-test");
     require("spec/PreferencesBase-test");
     require("spec/ProjectModel-test");
+    require("spec/QuickSearchField-test");
     // require("spec/CodeHint-test");
     // require("spec/CodeInspection-test");
     // require("spec/CSSInlineEdit-test");
@@ -83,7 +84,6 @@ define(function (require, exports, module) {
     // require("spec/PreferencesManager-test");
     // require("spec/ProjectManager-test");
     // require("spec/QuickOpen-test");
-    // require("spec/QuickSearchField-test");
     // require("spec/RemoteFunctions-test");
     // require("spec/SpecRunnerUtils-test");
     // require("spec/StringMatch-test");
