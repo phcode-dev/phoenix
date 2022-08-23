9c0d99195c55281fca59db6db092ac5f36b47649
Arun Bose <arun@core.ai>
2022-08-23 12:12:20 +0530

    refactor: quickview with async preview providers

--- src/extensions/default/QuickView/ImagePreviewProvider.js
+++ src/extensions/default/QuickView/ImagePreviewProvider.js
@@ -54,135 +54,137 @@ define(function (require, exports, module) {
 
     function getQuickView(editor, pos, token, line) {
 
-        // Check for image name
-        let urlRegEx = /url\(([^\)]*)\)/gi,
-            tokenString,
-            urlMatch;
+        return new Promise((resolve, reject)=>{
+            // Check for image name
+            let urlRegEx = /url\(([^\)]*)\)/gi,
+                tokenString,
+                urlMatch;
 
-        if (token.type === "string") {
-            tokenString = token.string;
-        } else {
-            urlMatch = urlRegEx.exec(line);
-            while (urlMatch) {
-                if (pos.ch < urlMatch.index) {
-                    // match is past cursor, so stop looping
-                    break;
-                } else if (pos.ch <= urlMatch.index + urlMatch[0].length) {
-                    tokenString = urlMatch[1];
-                    break;
-                }
+            if (token.type === "string") {
+                tokenString = token.string;
+            } else {
                 urlMatch = urlRegEx.exec(line);
+                while (urlMatch) {
+                    if (pos.ch < urlMatch.index) {
+                        // match is past cursor, so stop looping
+                        break;
+                    } else if (pos.ch <= urlMatch.index + urlMatch[0].length) {
+                        tokenString = urlMatch[1];
+                        break;
+                    }
+                    urlMatch = urlRegEx.exec(line);
+                }
             }
-        }
 
-        if (!tokenString) {
-            return null;
-        }
+            if (!tokenString) {
+                reject();
+                return;
+            }
 
-        // Strip leading/trailing quotes, if present
-        tokenString = tokenString.replace(/(^['"])|(['"]$)/g, "");
+            // Strip leading/trailing quotes, if present
+            tokenString = tokenString.replace(/(^['"])|(['"]$)/g, "");
 
-        let sPos, ePos;
-        let docPath = editor.document.file.fullPath;
-        let imgPath;
+            let sPos, ePos;
+            let docPath = editor.document.file.fullPath;
+            let imgPath;
 
-        // Determine whether or not this URL/path is likely to be an image.
-        let parsed = PathUtils.parseUrl(tokenString);
-        // If the URL has a protocol, check if it's one of the supported protocols
-        let hasProtocol = parsed.protocol !== "" && validProtocols.indexOf(parsed.protocol.trim().toLowerCase()) !== -1;
-        let ext = parsed.filenameExtension.replace(/^\./, '');
-        let language = LanguageManager.getLanguageForExtension(ext);
-        let id = language && language.getId();
-        let isImage = id === "image" || id === "svg";
-        let loadFromDisk = null;
+            // Determine whether or not this URL/path is likely to be an image.
+            let parsed = PathUtils.parseUrl(tokenString);
+            // If the URL has a protocol, check if it's one of the supported protocols
+            let hasProtocol = parsed.protocol !== "" && validProtocols.indexOf(parsed.protocol.trim().toLowerCase()) !== -1;
+            let ext = parsed.filenameExtension.replace(/^\./, '');
+            let language = LanguageManager.getLanguageForExtension(ext);
+            let id = language && language.getId();
+            let isImage = id === "image" || id === "svg";
+            let loadFromDisk = null;
 
-        // Use this URL if this is an absolute URL and either points to a
-        // filename with a known image extension, or lacks an extension (e.g.,
-        // a web service that returns an image). Honour the extensionlessImagePreview
-        // preference as well in the latter case.
-        if (hasProtocol && (isImage || (!ext && extensionlessImagePreview))) {
-            imgPath = tokenString;
-        }
-        // Use this filename if this is a path with a known image extension.
-        else if (!hasProtocol && isImage) {
-            imgPath = '';
-            loadFromDisk = window.path.normalize(FileUtils.getDirectoryPath(docPath) + tokenString);
-        }
+            // Use this URL if this is an absolute URL and either points to a
+            // filename with a known image extension, or lacks an extension (e.g.,
+            // a web service that returns an image). Honour the extensionlessImagePreview
+            // preference as well in the latter case.
+            if (hasProtocol && (isImage || (!ext && extensionlessImagePreview))) {
+                imgPath = tokenString;
+            }
+            // Use this filename if this is a path with a known image extension.
+            else if (!hasProtocol && isImage) {
+                imgPath = '';
+                loadFromDisk = window.path.normalize(FileUtils.getDirectoryPath(docPath) + tokenString);
+            }
 
-        if (!loadFromDisk && !imgPath) {
-            return null;
-        }
+            if (!loadFromDisk && !imgPath) {
+                reject();
+                return;
+            }
 
-        if (urlMatch) {
-            sPos = {line: pos.line, ch: urlMatch.index};
-            ePos = {line: pos.line, ch: urlMatch.index + urlMatch[0].length};
-        } else {
-            sPos = {line: pos.line, ch: token.start};
-            ePos = {line: pos.line, ch: token.end};
-        }
+            if (urlMatch) {
+                sPos = {line: pos.line, ch: urlMatch.index};
+                ePos = {line: pos.line, ch: urlMatch.index + urlMatch[0].length};
+            } else {
+                sPos = {line: pos.line, ch: token.start};
+                ePos = {line: pos.line, ch: token.end};
+            }
 
-        let imgPreview = "<div id='quick-view-image-preview'><div class='image-preview'>"          +
-            "    <img src=\"" + imgPath + "\">"    +
-            "</div></div>";
+            let $imgPreview = $("<div id='quick-view-image-preview'><div class='image-preview'>"          +
+                "    <img src=\"" + imgPath + "\">"    +
+                "</div></div>");
 
-        function showHandlerWithImageURL(imageURL) {
-            return new Promise((resolve, reject)=>{
+            function showHandlerWithImageURL(imageURL) {
                 // Hide the preview container until the image is loaded.
-                let $imageContainer = $("#quick-view-image-preview");
-                let img = $imageContainer.find("img");
+                let img = $imgPreview.find("img");
                 if(imageURL){
                     img[0].src = imageURL;
                 }
 
                 img.on("load", function () {
-                    $imageContainer
+                    $imgPreview
                         .append("<div class='img-size'>" +
                             this.naturalWidth + " &times; " + this.naturalHeight + " " + Strings.UNIT_PIXELS +
                             "</div>"
                         );
-                    resolve();
                 }).on("error", function (e) {
                     e.preventDefault();
-                    reject();
                 });
-            });
-        };
-
-        function _imageToDataURI(file, cb) {
-            let contentType = "data:image;base64,";
-            if(file.name.endsWith('.svg')){
-                contentType = "data:image/svg+xml;base64,";
             }
-            file.read({encoding: window.fs.BYTE_ARRAY_ENCODING}, function (err, content, encoding, stat) {
-                let base64 = window.btoa(
-                    new Uint8Array(content)
-                        .reduce((data, byte) => data + String.fromCharCode(byte), '')
-                );
-                let dataURL= contentType + base64;
-                cb(null, dataURL);
-            });
-        }
 
-        let showHandler = function () {
-            return new Promise((resolve, reject)=>{
-                if(loadFromDisk){
-                    let imageFile = FileSystem.getFileForPath(loadFromDisk);
-                    _imageToDataURI(imageFile, function (err, dataURL){
-                        showHandlerWithImageURL(dataURL).then(resolve).catch(reject);
-                    });
-                } else {
-                    showHandlerWithImageURL().then(resolve).catch(reject);
+            function _imageToDataURI(file, cb) {
+                let contentType = "data:image;base64,";
+                if(file.name.endsWith('.svg')){
+                    contentType = "data:image/svg+xml;base64,";
                 }
-            });
-        };
+                file.read({encoding: window.fs.BYTE_ARRAY_ENCODING}, function (err, content) {
+                    if(err){
+                        cb(err);
+                        return;
+                    }
+                    let base64 = window.btoa(
+                        new Uint8Array(content)
+                            .reduce((data, byte) => data + String.fromCharCode(byte), '')
+                    );
+                    let dataURL= contentType + base64;
+                    cb(null, dataURL);
+                });
+            }
 
-        return {
-            start: sPos,
-            end: ePos,
-            content: imgPreview,
-            onShow: showHandler,
-            _imgPath: imgPath || loadFromDisk
-        };
+            let previewPopup = {
+                start: sPos,
+                end: ePos,
+                content: $imgPreview,
+                _imgPath: imgPath || loadFromDisk
+            };
+
+            if(loadFromDisk){
+                let imageFile = FileSystem.getFileForPath(loadFromDisk);
+                _imageToDataURI(imageFile, function (err, dataURL){
+                    if(!err){
+                        showHandlerWithImageURL(dataURL);
+                        resolve(previewPopup);
+                    }
+                });
+            } else {
+                showHandlerWithImageURL();
+                resolve(previewPopup);
+            }
+        });
     }
 
     function setExtensionlessImagePreview(_extensionlessImagePreview, doNotSave) {
