957c4a753310e2438c31071e8ce4ba55dbfa1d0e
Arun Bose <arun@core.ai>
2022-08-25 20:12:13 +0530

    feat: add ExtensionWorker for generic extension web worker things

--- src/worker/ExtensionsWorker.js
+++ src/worker/ExtensionsWorker.js
@@ -21,35 +21,32 @@
 /*global Phoenix*/
 // @INCLUDE_IN_API_DOCS
 /**
- * Phoenix houses a file indexing worker which caches all cacheable files of a project in memory.
- * This module can be used to communicate with the Index and extend it by attaching new js worker scripts to the
- * indexing worker as discussed below. Any extension that works on a large number of files should use the indexing
- * worker cache to free up the main thread of heavy file access. This is similar to
- * [worker/ExtensionsWorker](ExtensionsWorker-API) but with a full file index.
+ * This is a generic web worker that is available for use by extensions to offload extension related tasks. This
+ * should only be used for performing small compute tasks and should not be used for long-running compute tasks.
  *
- * * Extensions are advised to use [worker/ExtensionsWorker](ExtensionsWorker-API) if they do not use the file index and
- *   just want to offload minor tasks.
+ * * Extensions are advised to use [worker/IndexingWorker](IndexingWorker-API) if they are performing large number of
+ *   file operations to utilize the file cache.
  * * Extensions performing large compute tasks should create their own worker and may use easy util methods in
  *   [worker/WorkerComm](WorkerComm-API) to communicate with the web worker.
  *
  * ## Import
  * ```js
  * // usage within extensions:
- * const IndexingWorker = brackets.getModule("worker/IndexingWorker");
+ * const ExtensionsWorker = brackets.getModule("worker/ExtensionsWorker");
  * ```
- * ## Extending the indexing worker
- * You can add your own custom scripts to the indexing worker by following the below example. Suppose you have an
+ * ## Extending the ExtensionsWorker
+ * You can add your own custom scripts to the ExtensionsWorker by following the below example. Suppose you have an
  * extension folder with the following structure:
  * ```
  * myExtensionFolder
  * │  my_worker.js // the script that you need to attach to the web worker
  * │  main.js
  * ```
- * In `main.js` extension module, we can import `my_worker.js` script into `IndexingWorker` by:
+ * In `main.js` extension module, we can import `my_worker.js` script into `ExtensionsWorker` by:
  * ```js
  * let ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
  * let workerPath = ExtensionUtils.getModulePath(module, "my_worker.js")
- * IndexingWorker.loadScriptInWorker(workerPath);
+ * ExtensionsWorker.loadScriptInWorker(workerPath);
  * ```
  *
  * Once the worker script is loaded with the above step:
@@ -60,28 +57,28 @@
  * A global constant `Phoenix.baseURL` is available in the worker context to get the base url from which phoenix was
  * launched.
  *
- * NB: You can use all util methods available in `worker/WorkerComm` as `IndexingWorker` internally uses `WorkerComm`
+ * NB: You can use all util methods available in `worker/WorkerComm` as `ExtensionsWorker` internally uses `WorkerComm`
  * to communicate with the underlying worker thread.
  *
- * @module worker/IndexingWorker
+ * @module worker/ExtensionsWorker
  */
 define(function (require, exports, module) {
     const WorkerComm = require("worker/WorkerComm"),
         EventDispatcher = require("utils/EventDispatcher");
 
-    const _FileIndexingWorker = new Worker(
-        `${Phoenix.baseURL}worker/file-Indexing-Worker-thread.js?debug=${window.logToConsolePref === 'true'}`);
+    const _ExtensionsWorker = new Worker(
+        `${Phoenix.baseURL}worker/extensions-worker-thread.js?debug=${window.logToConsolePref === 'true'}`);
 
-    if(!_FileIndexingWorker){
-        console.error("Could not load find in files worker! Search will be disabled.");
+    if(!_ExtensionsWorker){
+        console.error("Could not load Extensions worker! Some extensions may not work as expected.");
     }
     EventDispatcher.makeEventDispatcher(exports);
-    WorkerComm.createWorkerComm(_FileIndexingWorker, exports);
+    WorkerComm.createWorkerComm(_ExtensionsWorker, exports);
     /**
-     * To communicate between the IndexingWorker and Phoenix, the following methods are available:
+     * To communicate between the ExtensionsWorker and Phoenix, the following methods are available:
      * `loadScriptInWorker`, `execPeer`, `setExecHandler`, `triggerPeer` and other APIs described
      * in module `worker/WorkerComm`.
-     * The above methods can be used with either `IndexingWorker` reference within Phoenix
+     * The above methods can be used with either `ExtensionsWorker` reference within Phoenix
      * or the global `WorkerComm` reference within the Indexing worker. (See example below.)
      *
      * See [worker/WorkerComm](WorkerComm-API) for detailed API docs.
@@ -93,34 +90,8 @@ define(function (require, exports, module) {
      *     return "Hello Phoenix";
      *   });
      * // In Phoenix/extension
-     * let workerMessage = await IndexingWorker.execPeer("sayHello", "phoenix");
+     * let workerMessage = await ExtensionsWorker.execPeer("sayHello", "phoenix");
      * console.log(workerMessage); // prints "Hello Phoenix"
      * @name WorkerComm-APIS
      */
-
-    /**
-     * Raised when crawling started in the indexing worker.
-     * @event EVENT_CRAWL_STARTED
-     * @type {null}
-     */
-    exports.EVENT_CRAWL_STARTED = "crawlStarted";
-    /**
-     * Raised when crawling in progressing within the worker. The handler will receive the
-     * following properties as parameter.
-     * @event EVENT_CRAWL_PROGRESS
-     * @type {object}
-     * @property {number} processed The number of files cached till now.
-     * @property {number} total Number of files to cache.
-     */
-    exports.EVENT_CRAWL_PROGRESS = "crawlProgress";
-    /**
-     * Raised when crawling is complete within the worker. The handler will receive the
-     * following properties as parameter.
-     * @event EVENT_CRAWL_COMPLETE
-     * @type {object}
-     * @property {number} numFilesCached
-     * @property {number} cacheSizeBytes
-     * @property {number} crawlTimeMs in milliseconds.
-     */
-    exports.EVENT_CRAWL_COMPLETE = "crawlComplete";
 });
