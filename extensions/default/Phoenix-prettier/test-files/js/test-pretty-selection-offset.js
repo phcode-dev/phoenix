72388e5523375b87e80326c4fc8ae8d1bf6c2790
abose <abose@users.noreply.github.com>
2022-08-30 15:59:53 +0000

    deploy: 027c14e787ff0b45eb1e7c1d2c46eefb29b438bb

---
+++ extensions/default/Phoenix-prettier/test-files/js/test-pretty-selection-offset.js
@@ -0,0 +1,7 @@
+function hello(){console.log("hello");}
+
+function world(){console.log("world");}
+
+        function yo() {
+            console.log("yo");
+        }
---
+++ src/extensions/default/Phoenix-prettier/test-files/js/test.js
@@ -0,0 +1,6 @@
+function hello(){console.log("hello");}
+
+function world(){console.log("world");}
+
+        function yo(){
+        console.log("yo");}
