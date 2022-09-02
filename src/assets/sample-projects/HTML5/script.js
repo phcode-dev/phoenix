505a9e08b1e1e497ca4d8a1c7dbc58fa00dbef3c
Arun Bose <arun@core.ai>
2022-09-02 18:13:22 +0530

    refactor: beautify default and sample projects

--- src/assets/sample-projects/HTML5/script.js
+++ src/assets/sample-projects/HTML5/script.js
@@ -1,5 +1,6 @@
 let numButtonClicks = 0;
 function buttonClicked() {
     numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent = "Button Clicked times: " + numButtonClicks;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
 }
