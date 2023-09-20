078e65d3a2453c38bd627f27c9c79f1133b14905
abose <arun@core.ai>
2023-09-20 14:10:11 +0530

    fix: playright test failures opens browser to show failures in actions

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ const config = {
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: "list",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
