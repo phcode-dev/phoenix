60bc6d6fddd8dd21723802955d7072c445f4da28
abose <arun@core.ai>
2023-09-20 12:28:04 +0530

    test: reporters will now log to GitHub action console

--- tests/unit-test-suite.spec.js
+++ tests/unit-test-suite.spec.js
@@ -5,6 +5,15 @@ test("Execute all unit tests", async ({ page }) => {
     await page.goto(
         "http://localhost:5000/test/SpecRunner.html?spec=all&category=unit"
     );
+
+    // Expose a function to the page
+    await page.exposeFunction('testRunnerLogToConsole', (...args) => {
+        console.log(...args);
+    });
+    await page.exposeFunction('testRunnerErrorToConsole', (...args) => {
+        console.error(...args);
+    });
+
     // wait for spec runner to complete
     await page.waitForFunction(() => window.playWrightRunComplete);
     const result = await page.evaluate(() => {
