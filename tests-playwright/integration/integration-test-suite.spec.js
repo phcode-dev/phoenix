b861b9ba5234fe19ae22058df1ef4a6c637cc618
abose <arun@core.ai>
2023-10-02 18:29:19 +0530

    test: enable integration, LegacyInteg and mainview tests in playwright

--- tests-playwright/integration/integration-test-suite.spec.js
+++ tests-playwright/integration/integration-test-suite.spec.js
@@ -1,10 +1,9 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("Execute LegacyInteg tests", async ({ page }) => {
-    await page.goto(
-        "http://localhost:5000/test/SpecRunner.html?spec=all&category=LegacyInteg"
-    );
+async function execTests(page, url) {
+    await page.setViewportSize({ width: 1566, height: 1024 });
+    await page.goto(url);
 
     // Expose a function to the page
     await page.exposeFunction('testRunnerLogToConsole', (...args) => {
@@ -20,4 +19,22 @@ test("Execute LegacyInteg tests", async ({ page }) => {
         return Promise.resolve(window.testResults);
     });
     expect(result.errors).toStrictEqual({});
+}
+
+test("Execute integration tests", async ({ page }) => {
+    await execTests(page, "http://localhost:5000/test/SpecRunner.html?spec=all&category=integration");
 });
+
+test("Execute LegacyInteg tests", async ({ page }) => {
+    await execTests(page, "http://localhost:5000/test/SpecRunner.html?spec=all&category=LegacyInteg");
+});
+
+test("Execute mainview tests", async ({ page }) => {
+    await execTests(page, "http://localhost:5000/test/SpecRunner.html?spec=all&category=mainview");
+});
+
+// unfortunately live preview tests doesnt work in playwright :(
+// service workers are supported in playwright, debug this
+// test("Execute livepreview tests", async ({ page }) => {
+//     await execTests(page, "http://localhost:5000/test/SpecRunner.html?spec=all&category=livepreview");
+// });
