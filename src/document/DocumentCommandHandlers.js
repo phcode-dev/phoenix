fb5f456c1739e5129fce37831c02b94f6dd68585
abose <arun@core.ai>
2024-01-30 13:41:25 +0530

    fix: mac open with not working for the first time

--- src/document/DocumentCommandHandlers.js
+++ src/document/DocumentCommandHandlers.js
@@ -1633,8 +1633,7 @@ define(function (require, exports, module) {
 
     async function _singleInstanceHandler(args) {
         const isPrimary = await Phoenix.app.isPrimaryDesktopPhoenixWindow();
-        const bootTimeMacOsFlag = (args[0] === "macOSBootTimeDeepLink");
-        if(!bootTimeMacOsFlag && !isPrimary){
+        if(!isPrimary){
             // only primary phoenix windows can open a new window, else every window is going to make its own
             // window and cause a runaway phoenix window explosion.
             return;
