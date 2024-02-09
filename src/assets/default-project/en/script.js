d0a1f3c1d304dfe156ed84cbfc45423a0f16359c
abose <arun@core.ai>
2024-02-09 16:46:16 +0530

    chore: increase resolution on video in default project index.html

--- src/assets/default-project/en/script.js
+++ src/assets/default-project/en/script.js
@@ -2,3 +2,13 @@ function onPageLoaded() {
     // Write your javascript code here
     console.log("page loaded");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Listen for clicks on elements with the class 'play-button'
+    document.querySelectorAll('.play-button').forEach(function(button) {
+        button.addEventListener('click', function() {
+            // When a play button is clicked, simulate a click on the <a> tag within the same .video-container
+            this.parentNode.querySelector('a').click();
+        });
+    });
+});
