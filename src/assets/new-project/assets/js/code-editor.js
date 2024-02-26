f2f44a51028a3f6aa722867234cbd2c54094a982
abose <arun@core.ai>
2024-02-26 10:06:04 +0530

    chore: chnage bin icon to close icon for remove from recent projects in new project win

--- src/assets/new-project/assets/js/code-editor.js
+++ src/assets/new-project/assets/js/code-editor.js
@@ -41,19 +41,9 @@ function _createRecentProjectCard(fullPath, displayLocation, nodeId, tabIndex) {
             <div class="project-name">
                 ${newProjectExtension.path.basename(fullPath)}
             </div>
-            <button class="remove-btn" onclick="removeProject('${fullPath}');_recentProjectMetric('remove');"
-            style="${removeBtnDisableStyle}">
-                <svg width="16" height="16" viewBox="0 0 14 14" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.75 3.5H2.91667H12.25" stroke="#D0D0D0" stroke-linecap="round"
-                          stroke-linejoin="round"/>
-                    <path d="M4.6665 3.50008V2.33341C4.6665 2.024 4.78942 1.72725 5.00821 1.50846C5.22701 1.28966 5.52375 1.16675 5.83317 1.16675H8.1665C8.47592 1.16675 8.77267 1.28966 8.99146 1.50846C9.21026 1.72725 9.33317 2.024 9.33317 2.33341V3.50008M11.0832 3.50008V11.6667C11.0832 11.9762 10.9603 12.2729 10.7415 12.4917C10.5227 12.7105 10.2259 12.8334 9.91651 12.8334H4.08317C3.77375 12.8334 3.47701 12.7105 3.25821 12.4917C3.03942 12.2729 2.9165 11.9762 2.9165 11.6667V3.50008H11.0832Z"
-                          stroke="#D0D0D0" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M5.8335 6.41675V9.91675" stroke="#D0D0D0" stroke-linecap="round"
-                          stroke-linejoin="round"/>
-                    <path d="M8.1665 6.41675V9.91675" stroke="#D0D0D0" stroke-linecap="round"
-                          stroke-linejoin="round"/>
-                </svg>
+            <button class="remove-btn recent-project-remove" onclick="removeProject('${fullPath}');_recentProjectMetric('remove');"
+            style="${removeBtnDisableStyle};" title="${Strings.REMOVE_FROM_RECENT_PROJECTS}">
+                <i class="fa-solid fa-xmark"></i>
             </button>
         </a>
         <div style="overflow: hidden;">
