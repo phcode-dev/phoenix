259a0956ca1a70a70a60875a11f9a99ac807357d
abose <arun@core.ai>
2024-04-30 15:18:20 +0530

    test: show custom live preview banner on switching off/on custom server

--- test/spec/LiveDevelopmentCustomServer-test.js
+++ test/spec/LiveDevelopmentCustomServer-test.js
@@ -724,6 +724,12 @@ define(function (require, exports, module) {
                 return testWindow.$(".live-preview-custom-banner").is(":visible");
             }, "banner to show");
 
+            // banner should now go away on custom server off
+            PreferencesManager.set(PREFERENCE_PROJECT_SERVER_ENABLED, false, PreferencesManager.PROJECT_SCOPE);
+            await awaitsFor(()=>{
+                return !testWindow.$(".live-preview-custom-banner").is(":visible");
+            }, "banner to show");
+
             await endPreviewSession();
         }, 30000);
 
