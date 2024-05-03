ec9f76aea75388a75f7443f2ff3346a88794d631
abose <arun@core.ai>
2024-05-03 23:30:02 +0530

    test: node extenstion loading integration test

--- test/spec/ExtensionLoader-test-files/extension/main.js
+++ test/spec/ExtensionLoader-test-files/extension/main.js
@@ -4,12 +4,19 @@ define(function (require, exports, module) {
     "use strict";
 
     // Brackets modules
-    var AppInit = brackets.getModule("utils/AppInit");
+    const NodeConnector = brackets.getModule("NodeConnector");
+    const AppInit = brackets.getModule("utils/AppInit");
 
     // Initialize extension once shell is finished initializing.
     AppInit.appReady(function () {
         console.log("hello world");
         window.extensionLoaderTestExtensionLoaded = true;
+        if(Phoenix.isNativeApp) {
+            const nodeConnector = NodeConnector.createNodeConnector("extension_connector_1", exports);
+            window._testNodeExt = function () {
+                return nodeConnector.execPeer("echoTest", "yo!");
+            };
+        }
     });
 
 });
