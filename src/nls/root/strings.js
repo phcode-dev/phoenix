d6b0cce15a2be63ca4145d22931e980c7acb1deb
abose <arun@core.ai>
2024-09-17 11:04:55 +0530

    feat: get more themes button in themes dialog for discoverability

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -677,6 +677,7 @@ define({
 
     // Strings for themes-settings.html and themes-general.html
     "CURRENT_THEME": "Current Theme",
+    "GET_MORE_THEMES": "Get More...",
     "USE_THEME_SCROLLBARS": "Use Theme Scrollbars",
     "FONT_SIZE": "Font Size",
     "FONT_FAMILY": "Font Family",
