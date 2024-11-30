8f421bb6985fa06d304b845958da5b55c9638835
abose <arun@core.ai>
2024-11-30 12:12:54 +0530

    fix: default project beautify code

--- src/assets/default-project/en/script.js
+++ src/assets/default-project/en/script.js
@@ -3,12 +3,12 @@ function onPageLoaded() {
     console.log("page loaded");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Listen for clicks on elements with the class 'play-button'
-    document.querySelectorAll('.play-button').forEach(function(button) {
-        button.addEventListener('click', function() {
+    document.querySelectorAll(".play-button").forEach(function (button) {
+        button.addEventListener("click", function () {
             // When a play button is clicked, simulate a click on the <a> tag within the same .video-container
-            this.parentNode.querySelector('a').click();
+            this.parentNode.querySelector("a").click();
         });
     });
 });
