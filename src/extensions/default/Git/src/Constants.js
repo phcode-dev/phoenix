4055fedb8b87af3df9aa23e4b1ecb58222b571a7
abose <arun@core.ai>
2025-01-16 16:59:11 +0530

    test: git integ tests scaffolding and initial test

--- src/extensions/default/Git/src/Constants.js
+++ src/extensions/default/Git/src/Constants.js
@@ -19,8 +19,11 @@
  */
 
 define(function (require, exports) {
+    const Commands = brackets.getModule("command/Commands"),
+        Menus = brackets.getModule("command/Menus");
+
     exports.GIT_STRING_UNIVERSAL = "Git";
-    exports.GIT_SUB_MENU = "git-submenu";
+    exports.GIT_SUB_MENU = Menus.SubMenuIds.GIT_SUB_MENU;
 
     // Menus
     exports.GIT_PANEL_CHANGES_CMENU = "git-panel-changes-cmenu";
@@ -28,31 +31,31 @@ define(function (require, exports) {
     exports.GIT_PANEL_OPTIONS_CMENU = "git-panel-options-cmenu";
 
     // commands
-    exports.CMD_GIT_INIT = "git-init";
-    exports.CMD_GIT_CLONE = "git-clone";
-    exports.CMD_GIT_CLONE_WITH_URL = "git-clone-url";
-    exports.CMD_GIT_SETTINGS_COMMAND_ID = "git-settings";
-    exports.CMD_GIT_CLOSE_UNMODIFIED = "git-close-unmodified-files";
-    exports.CMD_GIT_CHECKOUT = "git-checkout";
-    exports.CMD_GIT_RESET_HARD = "git-reset-hard";
-    exports.CMD_GIT_RESET_SOFT = "git-reset-soft";
-    exports.CMD_GIT_RESET_MIXED = "git-reset-mixed";
-    exports.CMD_GIT_TOGGLE_PANEL = "git-toggle-panel";
-    exports.CMD_GIT_GOTO_NEXT_CHANGE = "git-gotoNextChange";
-    exports.CMD_GIT_GOTO_PREVIOUS_CHANGE = "git-gotoPrevChange";
-    exports.CMD_GIT_COMMIT_CURRENT = "git-commitCurrent";
-    exports.CMD_GIT_COMMIT_ALL = "git-commitAll";
-    exports.CMD_GIT_FETCH = "git-fetch";
-    exports.CMD_GIT_PULL = "git-pull";
-    exports.CMD_GIT_PUSH = "git-push";
-    exports.CMD_GIT_REFRESH = "git-refresh";
-    exports.CMD_GIT_TAG = "git-tag";
-    exports.CMD_GIT_DISCARD_ALL_CHANGES = "git-discard-all-changes";
-    exports.CMD_GIT_UNDO_LAST_COMMIT = "git-undo-last-commit";
-    exports.CMD_GIT_CHANGE_USERNAME = "git-change-username";
-    exports.CMD_GIT_CHANGE_EMAIL = "git-change-email";
-    exports.CMD_GIT_GERRIT_PUSH_REF = "git-gerrit-push_ref";
-    exports.CMD_GIT_AUTHORS_OF_SELECTION = "git-authors-of-selection";
-    exports.CMD_GIT_AUTHORS_OF_FILE = "git-authors-of-file";
-    exports.CMD_GIT_TOGGLE_UNTRACKED = "git-toggle-untracked";
+    exports.CMD_GIT_INIT = Commands.CMD_GIT_INIT;
+    exports.CMD_GIT_CLONE = Commands.CMD_GIT_CLONE;
+    exports.CMD_GIT_CLONE_WITH_URL = Commands.CMD_GIT_CLONE_WITH_URL;
+    exports.CMD_GIT_SETTINGS_COMMAND_ID = Commands.CMD_GIT_SETTINGS_COMMAND_ID;
+    exports.CMD_GIT_CLOSE_UNMODIFIED = Commands.CMD_GIT_CLOSE_UNMODIFIED;
+    exports.CMD_GIT_CHECKOUT = Commands.CMD_GIT_CHECKOUT;
+    exports.CMD_GIT_RESET_HARD = Commands.CMD_GIT_RESET_HARD;
+    exports.CMD_GIT_RESET_SOFT = Commands.CMD_GIT_RESET_SOFT;
+    exports.CMD_GIT_RESET_MIXED = Commands.CMD_GIT_RESET_MIXED;
+    exports.CMD_GIT_TOGGLE_PANEL = Commands.CMD_GIT_TOGGLE_PANEL;
+    exports.CMD_GIT_GOTO_NEXT_CHANGE = Commands.CMD_GIT_GOTO_NEXT_CHANGE;
+    exports.CMD_GIT_GOTO_PREVIOUS_CHANGE = Commands.CMD_GIT_GOTO_PREVIOUS_CHANGE;
+    exports.CMD_GIT_COMMIT_CURRENT = Commands.CMD_GIT_COMMIT_CURRENT;
+    exports.CMD_GIT_COMMIT_ALL = Commands.CMD_GIT_COMMIT_ALL;
+    exports.CMD_GIT_FETCH = Commands.CMD_GIT_FETCH;
+    exports.CMD_GIT_PULL = Commands.CMD_GIT_PULL;
+    exports.CMD_GIT_PUSH = Commands.CMD_GIT_PUSH;
+    exports.CMD_GIT_REFRESH = Commands.CMD_GIT_REFRESH;
+    exports.CMD_GIT_TAG = Commands.CMD_GIT_TAG;
+    exports.CMD_GIT_DISCARD_ALL_CHANGES = Commands.CMD_GIT_DISCARD_ALL_CHANGES;
+    exports.CMD_GIT_UNDO_LAST_COMMIT = Commands.CMD_GIT_UNDO_LAST_COMMIT;
+    exports.CMD_GIT_CHANGE_USERNAME = Commands.CMD_GIT_CHANGE_USERNAME;
+    exports.CMD_GIT_CHANGE_EMAIL = Commands.CMD_GIT_CHANGE_EMAIL;
+    exports.CMD_GIT_GERRIT_PUSH_REF = Commands.CMD_GIT_GERRIT_PUSH_REF;
+    exports.CMD_GIT_AUTHORS_OF_SELECTION = Commands.CMD_GIT_AUTHORS_OF_SELECTION;
+    exports.CMD_GIT_AUTHORS_OF_FILE = Commands.CMD_GIT_AUTHORS_OF_FILE;
+    exports.CMD_GIT_TOGGLE_UNTRACKED = Commands.CMD_GIT_TOGGLE_UNTRACKED;
 });
