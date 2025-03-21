b44b2047e37625654032066f259d1f8c7dee32eb
Pluto <devvaannsh@gmail.com>
2025-03-21 14:39:08 +0530

    chore: add license

--- src/extensionsIntegrated/TabBar/overflow.js
+++ src/extensionsIntegrated/TabBar/overflow.js
@@ -1,3 +1,30 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+
+/*
+ * This file houses the functionality for overflow tabs,
+ * the overflow tabs appear when there are too many tabs in a pane
+ * overflow button when clicked opens up a dropdown menu which displays all the hidden tabs
+ * and allows the user to open them, close them and to achieve other functionalities from there
+ */
 /* eslint-disable no-invalid-this */
 define(function (require, exports, module) {
 
