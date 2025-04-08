01337a803c8cc228120d1addd70c602b4c4302d7
abose <arun@core.ai>
2025-04-08 16:37:08 +0530

    fix: choppy cursor selection performance issue

--- src/extensions/default/Git/src/GutterManager.js
+++ src/extensions/default/Git/src/GutterManager.js
@@ -49,6 +49,11 @@ define(function (require, exports) {
 
     function _cursorActivity(_evt, editor){
         // this is to prevent a gutter gap in the active line if there is no color on this line.
+        if (editor.hasSelection()){
+            // we dont show the gutter gap color when there is a selection. also adding dummy gutter is expensive
+            // and make test selection with cursor choppy
+            return;
+        }
         _addDummyGutterMarkerIfNotExist(editor._codeMirror, editor.getCursorPos().line);
     }
 
