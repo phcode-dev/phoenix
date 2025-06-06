d42d14cf346839b29310138d584c1bfdea68327f
Pluto <devvaannsh@gmail.com>
2025-06-07 02:22:34 +0530

    feat: add license to all files

--- src/extensionsIntegrated/CustomSnippets/snippetsList.js
+++ src/extensionsIntegrated/CustomSnippets/snippetsList.js
@@ -1,4 +1,24 @@
 /*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+
+/*
  * This file handles the display and management of the snippets list that is shown in the UI
  * Note: when there are no snippets present, a message like no snippets are added yet is shown. refer to the html file
  */
