70db3f81ad8c58407009bdd1f64d3d2a481213dc
abose <arun@core.ai>
2025-09-15 14:09:20 +0530

    fix: trial data handling logic for promo and pro activation
    1. Corrected trial activation logic to properly detect corrupted data, handle
        existing trials, and avoid granting trials on tampering or invalid state.
    2. Enhanced robustness by explicitly handling failure cases (e.g., JSON parse
         errors, invalid signatures).

--- src/phoenix/trust_ring.js
+++ src/phoenix/trust_ring.js
@@ -179,7 +179,8 @@ function _selectKeys() {
 }
 
 const CRED_KEY_API = "API_KEY";
-const CRED_KEY_ENTITLEMENTS = "ENTITLEMENTS_GRANT_KEY";
+const CRED_KEY_PROMO = "PROMO_GRANT_KEY";
+const SIGNATURE_SALT_KEY = "SIGNATURE_SALT_KEY";
 const { key, iv } = _selectKeys();
 // this key is set at boot time as a truct base for all the core components before any extensions are loaded.
 // just before extensions are loaded, this key is blanked. This can be used by core modules to talk with other
@@ -187,7 +188,8 @@ const { key, iv } = _selectKeys();
 // KernalModeTrust should only be available within all code that loads before the first default/any extension.
 window.KernalModeTrust = {
     CRED_KEY_API,
-    CRED_KEY_ENTITLEMENTS,
+    CRED_KEY_PROMO,
+    SIGNATURE_SALT_KEY,
     aesKeys: { key, iv },
     setCredential,
     getCredential,
